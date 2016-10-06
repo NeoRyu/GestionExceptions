fbf87e6626b044c1bcc5599592cc692d534f2bad
NeoRyu <coupez.frederic@gmail.com>
2016-10-06 15:31:25 +0200

    vALPHA (demo autocorrection)

--- src/shell/ErrorGestionSys.java
+++ src/shell/ErrorGestionSys.java
@@ -58,7 +58,7 @@ public class ErrorGestionSys {
 	
 // A --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(ArithmeticException e){
-	    System.out.println("ArithmeticException : "+e.toString());
+	    System.out.println("ArithmeticException : \n"+e.toString());
 	}	// Exception levée lorsqu'une condition arithmétique exceptionnelle a eu lieu.
 	
 	public void gestionException(ArrayIndexOutOfBoundsException e){
@@ -72,187 +72,187 @@ public class ErrorGestionSys {
 	}	// Indique qu'une tentative a été faite pour stocker le mauvais type d'objet dans un tableau d'objets.
 	
 	public void gestionException(AuthenticationException e){
-	    System.out.println("AuthenticationException : "+e.toString());
+	    System.out.println("AuthenticationException : \n"+e.toString());
 	}	// Exceptions liées à un objet d'authentification étant invalide pour une raison quelconque.
 	
 // C --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(CharConversionException e){
-	    System.out.println("CharConversionException : "+e.toString());
+	    System.out.println("CharConversionException : \n"+e.toString());
 	}	// Ceci est une classe de base pour les exceptions de conversion de caractères.
 	
 	public void gestionException(ClassCastException e){
-	    System.out.println("ClassCastException : "+e.toString());
+	    System.out.println("ClassCastException : \n"+e.toString());
 	}	// Indique que le code a tenté de throw un objet à une sous-classe dont il n'est pas une instance.
 	
 	public void gestionException(ClassNotFoundException e){
-	    System.out.println("ClassNotFoundException : "+e.toString());
+	    System.out.println("ClassNotFoundException : \n"+e.toString());
 	}	// Exception soulevée si une erreur est survenue lors de la tentative de charger une classe 
 	
 	public void gestionException(CloneNotSupportedException e){
-	    System.out.println("CloneNotSupportedException : "+e.toString());
+	    System.out.println("CloneNotSupportedException : \n"+e.toString());
 	}	// Indique que la méthode clone dans la classe Object a été appelée à cloner un objet, 
 		// mais que la classe de l'objet ne met pas en oeuvre d'interface Clonable.
 	
 // E --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(EOFException  e){
-	    System.out.println("EOFException  : "+e.toString());
+	    System.out.println("EOFException  : \n"+e.toString());
 	}	// Signale une fin innatendue de fichier ou de flux.
 	
 	public void gestionException(EnumConstantNotPresentException  e){
-	    System.out.println("EnumConstantNotPresentException  : "+e.toString());
+	    System.out.println("EnumConstantNotPresentException  : \n"+e.toString());
 	}	// Levée lorsqu'une application tente d'accéder à une constante d'une enum par nom,
 		// mais que le type enum ne contient pas de constante avec le nom spécifié.
 	
 // F --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(FileNotFoundException  e){
-	    System.out.println("FileNotFoundException  : "+e.toString());
+	    System.out.println("FileNotFoundException  : \n"+e.toString());
 	}	// Echec lors de la tentative d'ouvrerture d'une fichier désigné par un chemin d'accès spécifié.
 	
 // I --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(IllegalAccessException e){
-	    System.out.println("IllegalAccessException : "+e.toString());
+	    System.out.println("IllegalAccessException : \n"+e.toString());
 	}	// Lancée lorsqu'une application tente de créer reflexivement une instance (autre qu'un tableau), 
 		// définir ou obtenir un champ, ou invoquer une méthode ; mais que la méthode en cours d'exécution 
 		// n'a pas accès à la définition de la classe, au champ spécifié, à la méthode ou au constructeur.
 	
 	public void gestionException(IllegalArgumentException e) {
-	    System.out.println("IllegalArgumentException : "+e.toString());
+	    System.out.println("IllegalArgumentException : \n"+e.toString());
 	}	// Levée lorsque qu'une méthode a reçue un argument illégal ou inapproprié.
 	
 	public void gestionException(IllegalMonitorStateException e) {
-	    System.out.println("IllegalMonitorStateException : "+e.toString());
+	    System.out.println("IllegalMonitorStateException : \n"+e.toString());
 	}	// Indique qu'un thread a tenté d'attendre sur le moniteur d'un objet, ou d'informer les autres threads
 		// en attente sur le moniteur d'un objet sans posséder de droits sur le moniteur spécifié.
 	
 	public void gestionException(IllegalStateException e) {
-	    System.out.println("IllegalStateException : "+e.toString());
+	    System.out.println("IllegalStateException : \n"+e.toString());
 	}	// L'environnement Java ou l'application Java ne sont pas dans un etat approprie pour l'opération demandée.
 	
 	public void gestionException(IllegalThreadStateException e) {
-	    System.out.println("IllegalThreadStateException : "+e.toString());
+	    System.out.println("IllegalThreadStateException : \n"+e.toString());
 	}	// Indique qu'un thread ne se trouve pas dans un état approprié pour l'opération demandée.
 	
 	public void gestionException(IndexOutOfBoundsException e) {
-	    System.out.println("IndexOutOfBoundsException : "+e.toString());
+	    System.out.println("IndexOutOfBoundsException : \n"+e.toString());
 	}	// L'indice est hors de portée (par exemple for(int i = 0;  ; i++))
 	 	
 	public void gestionException(InstantiationException e){
-	    System.out.println("InstantiationException : "+e.toString());
+	    System.out.println("InstantiationException : \n"+e.toString());
 	}	// Levée lorsqu'une instanciation echoue (diverses raisons possibles...)
 		// Exemples : la classe n'a pas de constructeur, l'objet de classe représente une
 		// classe abstraite, une interface, une classe de tableau, un type primitif, null, ... 
 	
 	public void gestionException(InterruptedException e) {
-	    System.out.println("InterruptedException : "+e.toString());
+	    System.out.println("InterruptedException : \n"+e.toString());
 	}	// Levée lorsqu'un thread attend, dors, ou est occupé, et que le thread est interrompu.
 	
 	public void gestionException(InterruptedIOException e){
-	    System.out.println("InterruptedIOException : "+e.toString());
+	    System.out.println("InterruptedIOException : \n"+e.toString());
 	}	// Signale qu'une opération d'Entrée/Sortie à été interrompue.
 	
 	public void gestionException(InvalidClassException e){
-	    System.out.println("InvalidClassException : "+e.toString());
+	    System.out.println("InvalidClassException : \n"+e.toString());
 	}	// Levée lorsque le moteur d'exécution de sérialisation détecte un problème avec une classe.
 	
 	public void gestionException(InvalidObjectException e){
-	    System.out.println("InvalidObjectException : "+e.toString());
+	    System.out.println("InvalidObjectException : \n"+e.toString());
 	}	// Indique qu'un ou plusieurs objets désérialisés ont échoués au test de validation.
 	
 	public void gestionException(IOException e){
-	    System.out.println("IOException : "+e.toString());
+	    System.out.println("IOException : \n"+e.toString());
 	}	// Un flux en entrée/sortie (I/O) a echoué ou a été interrompu
 	
 // N --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(NegativeArraySizeException e){
-	    System.out.println("NegativeArraySizeException : "+e.toString());
+	    System.out.println("NegativeArraySizeException : \n"+e.toString());
 	}	// Levée si une application tente de créer un tableau avec une taille négative.
 	
 	public void gestionException(NoSuchFieldException e){
-	    System.out.println("NoSuchFieldException : "+e.toString());
+	    System.out.println("NoSuchFieldException : \n"+e.toString());
 	}	// Signale que la classe n'a pas de champ avec le nom spécifié.
 	
 	public void gestionException(NoSuchMethodException e){
-	    System.out.println("NoSuchMethodException : "+e.toString());
+	    System.out.println("NoSuchMethodException : \n"+e.toString());
 	}	
 	
 	public void gestionException(NotActiveException e){
-	    System.out.println("NotActiveException : "+e.toString());
+	    System.out.println("NotActiveException : \n"+e.toString());
 	}	// Exception levée lorsque sérialisation ou désérialisation n'est pas active.
 	
 	public void gestionException(NotSerializableException e){
-	    System.out.println("NotSerializableException : "+e.toString());
+	    System.out.println("NotSerializableException : \n"+e.toString());
 	}	// Exceptions levée lorsqu'une instance nécessite d'avoir une interface Serializable.
 	
 	public void gestionException(NullPointerException e){
-	    System.out.println("NullPointerException : "+e.toString());
+	    System.out.println("NullPointerException : \n"+e.toString());
 	}	// Levée lorsqu'une application tente d'utiliser null dans un cas où un objet est nécessaire.
 	
 	public void gestionException(NumberFormatException e){
-	    System.out.println("NumberFormatException : "+e.toString());
+	    System.out.println("NumberFormatException : \n"+e.toString());
 	}	// Indique que l'application a tenté de convertir une chaîne dans l'un des types numériques, 
 		// mais que la chaîne n'a pas le format approprié.
 
 // O --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(ObjectStreamException e){
-	    System.out.println("ObjectStreamException : "+e.toString());
+	    System.out.println("ObjectStreamException : \n"+e.toString());
 	}	// Ceci est une superclasse de toutes les exceptions spécifiques à l'objet des classes Stream.
 	
 	public void gestionException(OptionalDataException e){
-	    System.out.println("OptionalDataException : "+e.toString());
+	    System.out.println("OptionalDataException : \n"+e.toString());
 	}	// Indiquant l'échec d'une opération de lecture d'un objet en raison de données primitives
 		// non lues, ou la fin des données appartenant à un objet sérialisé dans le flux.
 	
 // P --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(ParseException e){
-	    System.out.println("ParseException : "+e.toString());
+	    System.out.println("ParseException : \n"+e.toString());
 	}	// Signale une erreur innatendue lors de l'analyse.
 	
 // R --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(RuntimeException e){
-		System.out.println("RuntimeException : "+e.toString());
+		System.out.println("RuntimeException : \n"+e.toString());
 	}	// C'est la superclasse des exceptions qui peuvent être levées pendant le fonctionnement normal de la JMV.
 	
 // S --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(SecurityException e){
-	    System.out.println("SecurityException : "+e.toString());
+	    System.out.println("SecurityException : \n"+e.toString());
 	}	// Exceptions levée par le security manager pour indiquer une violation de sécurité.
 	
 	public void gestionException(StreamCorruptedException e){
-	    System.out.println("StreamCorruptedException : "+e.toString());
+	    System.out.println("StreamCorruptedException : \n"+e.toString());
 	}	// Levée lorsque les informations de contrôle lues à partir d'un flux d'objet violent les contrôles de cohérence interne.
 	
 	public void gestionException(StringIndexOutOfBoundsException e){
-		System.out.println("StringIndexOutOfBoundsException : "+e.toString());
+		System.out.println("StringIndexOutOfBoundsException : \n"+e.toString());
 	}	// Levée par des méthodes String pour indiquer qu'un index est négatif ou supérieur à la taille de la chaîne.
 	
 	public void gestionException(SyncFailedException e){
-	    System.out.println("SyncFailedException : "+e.toString());
+	    System.out.println("SyncFailedException : \n"+e.toString());
 	}	// Signale qu'une opération de synchronisation a échouée.
 
 
 // T --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(TypeNotPresentException e){
-		System.out.println("SecurityException : "+e.toString());
+		System.out.println("SecurityException : \n"+e.toString());
 	}	// Levée lorsqu'une application tente d'accéder à un type en utilisant une chaîne représentant le nom du type,
 		// mais pas de définition pour le type avec le nom spécifié introuvable.
 		
 // U --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(UnsupportedEncodingException e){
-	    System.out.println("UnsupportedEncodingException : "+e.toString());
+	    System.out.println("UnsupportedEncodingException : \n"+e.toString());
 	}	// Ce codage de caractères n'est pas pris en charge.
 	
 	public void gestionException(UnsupportedOperationException e){
-	    System.out.println("UnsupportedOperationException : "+e.toString());
+	    System.out.println("UnsupportedOperationException : \n"+e.toString());
 	}	// Exception levée pour indiquer que l'opération demandée n'est pas prise en charge.
 	
 	public void gestionException(UTFDataFormatException e){
-	    System.out.println("UTFDataFormatException : "+e.toString());
+	    System.out.println("UTFDataFormatException : \n"+e.toString());
 	}	// Signale qu'une chaîne malformée au format UTF-8 modifié a été lue dans un flux d'entrée de données
 		// ou par toute classe qui implémente l'interface d'entrée de données.
 	
 // W --------------------------------------------------------------------------------------------------------------------
 	public void gestionException(WriteAbortedException e){
-	    System.out.println("WriteAbortedException : "+e.toString());
+	    System.out.println("WriteAbortedException : \n"+e.toString());
 	}	// Signale que l'un des ObjectStreamExceptions a été lancé au cours d'une opération d'écriture.
 
 //-----------------------------------------------------------------------------------------------------------------------
--- src/shell/Principal.java
+++ src/shell/Principal.java
@@ -1,54 +1,87 @@
 package shell;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.lang.reflect.Method;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 import shell.ErrorRedirect;
 
 // CLASSE GENERANT DES ERREURS...
 public class Principal {
 	
-	// Obtention des object associés à cette classe
-
-	public static boolean viewFatalError = false;
-	public static boolean viewBloc = false;	
+	// INSTANCIATION DE LA CLASSE DE GESTION DES EXCEPTIONS SYSTEMES
 	public static ErrorRedirect Error;
 	
+	
+	// CONFIGURATION : true / false
+	public static boolean viewFatalError = false;
+	public static boolean viewBloc = false;	
+	
+	
+	// DEBUT DU PROGRAMME
 	public static void main(String[] args) {
+		
+		// CREATION D'UN LOGGER 
+		// (mettre LOG etc hors de la methode pour l'utiliser ailleurs)
+		Principal osef = new Principal();		
+		Logger LOG = Logger.getLogger(Principal.class.getName());	
+		
 		try {
 			System.out.println("CREATION D'ERREURS");
+			LOG.log(Level.INFO, "Démarrage de l'application");	
+			System.out.print("");
 			
 			// CREATION D'ERREURS SUR DIVERS PACKAGE :
 			
 			test3("");	// Exception Personnelle			
 			
-			test2();	// JAVA IO *Exception;
+			// false = erreur au premier tour puis resolution
+			// true = pas d'erreur (identique a resolution)
 			
-			test1();	// JAVA LANG *Exception;
+			test2(false);
 			
-			test0();	// JAVA LANG *Exception;
+			test1(false);
 			
-		} catch (Exception e) {
+			test0();
+			
+		} catch (Exception e) {	
+			System.out.println("\n"); 
+			LOG.log(Level.SEVERE, "Fermeture de l'application...");
+					   /* SEVERE (highest value)
+						* WARNING
+						* INFO
+						* CONFIG
+						* FINE
+						* FINER
+						* FINEST (lowest value)
+						*/
 			System.out.println("\nCALLBACK CATCH EXCEPTION : shell.Principal");
 			
 			shell.ErrorRedirect.Reflexivite();			
 			shell.ErrorRedirect.RedErreurs(e);
-			
-			System.out.println("Fermeture de l'application...");
 		}		
 	}
 	
 	
 	// AUCUNE ERREUR DANS CETTE METHODE
 	public static void test0() {
-		System.out.println("\n--------------------------\nMETHODE 0");
+		System.out.println("\n--------------------------\nMETHODE 4/4");
 		try {
 			if (viewBloc) 
 				System.out.println("TRY");		
 
-			System.out.println("\nICI TOUT CE PASSE BIEN !\n");
+			System.out.println("\n TOUT CE PASSE BIEN !\n");
 			
 		} catch (Exception e) {
 			if (viewBloc) 
 				System.out.println("CATCH");
+			
+			System.out.println("Oups, il ne devait pas y avoir d'erreur ici !");
 			
 			// Divers moyen d'afficher le message d'erreur (surcharge de methode)
 			shell.ErrorRedirect.Reflexivite();			
@@ -63,27 +96,38 @@ public class Principal {
 	}
 	
 	// ERREURS DE TYPE : JAVA.LANG.*Exception;
-	public static void test1() {
-		System.out.println("\n--------------------------\nMETHODE 1");
+	public static void test1(boolean error) {
+		System.out.println("\n--------------------------\nMETHODE 3/4");
+	
+		// Declaration hors du TRY pour etre accessible dans le CATCH :
 		int tab[] = new int[9];
+		
 		try {
 			if (viewBloc) 
 				System.out.println("TRY");	
 			
-			// ArrayIndexOutOfBoundsException			
-			for(int i = 0 ; i < 1 ; i++) {
-				i = i-1;
-				System.out.println(""+tab[10]);
+			// ArrayIndexOutOfBoundsException				
+			for(int i = 0 ; i < 9 ; i++) {
+				tab[i] = i;
+				if (!error)
+					if (i == 9)
+						System.out.println(""+tab[10]);
+				else
+					if (i == 9)
+						System.out.println(""+tab[9]);
 			}
 			
-			// NoSuchMethodException
-			shell.ErrorRedirect.Reflexivite();
-			Method method = ErrorRedirect.c.getClass().getMethod(ErrorRedirect.c, null);
-			ErrorRedirect.m = method.getName().toString();
-			System.out.println(ErrorRedirect.m);  
+			if (!error) {
+				// NoSuchMethodException
+				shell.ErrorRedirect.Reflexivite();
+				Method method = ErrorRedirect.c.getClass().getMethod(ErrorRedirect.c, null);
+				ErrorRedirect.m = method.getName().toString();
+				System.out.println(ErrorRedirect.m);  
+				// Genere normallement une exception
+			}
 			
 			// Si une erreur apparait comme prévue, le message suivant n'apparaitra pas :
-			System.out.println("\nCa roule mec ?\n");
+			System.out.println("\nHello World !!!\n");
 			
 		} catch (Exception e) {
 			if (viewBloc) 
@@ -92,31 +136,63 @@ public class Principal {
 			shell.ErrorRedirect.Reflexivite();			
 			shell.ErrorRedirect.RedErreurs(e,ErrorRedirect.c,ErrorRedirect.m);
 			
-			// SI VOUS MODIFIEZ MAIS EN RECREANT UNE SECONDE ERREUR :
-			if (viewFatalError) 
-				System.out.println("RESULTAT : "+tab[9]);
+			System.out.println("Oups, reessayont autrement !");
 
 		} finally {	//Traitements pour terminer proprement
 			if (viewBloc) 
-				System.out.println("FINALLY");				
+				System.out.println("FINALLY");	
+			
+			//RESOLUTION DE L'ERREUR
+			// SI VOUS MODIFIEZ MAIS EN RECREANT UNE SECONDE ERREUR :
+			if (viewFatalError) {
+				System.out.println("RESULTAT : "+tab[12]);
+			} else {
+				if (!error) {			
+					try {
+						test1(true);
+					} catch (Exception e) {
+						if (viewBloc) 
+							System.out.println("CATCH");
+						
+						shell.ErrorRedirect.Reflexivite();			
+						shell.ErrorRedirect.RedErreurs(e,ErrorRedirect.c,ErrorRedirect.m);
+					}
+				}	
+			}
 		}
 		if (viewBloc) 
 			System.out.println("SORTIE DU BLOC TRY / CATCH !");
 	}
 	
 	// ERREUR DE TYPE JAVA.IO.*Exception;
-	public static void test2() {
-		System.out.println("\n--------------------------\nMETHODE 2");
+	public static void test2(boolean error) {
+		System.out.println("\n--------------------------\nMETHODE 2/4");
 		try {
 			if (viewBloc) 
-				System.out.println("TRY");				
+				System.out.println("TRY");			
 			
-			// TODO : GENERER UNE ERREUR I/O ici (pourquoi pas serializable)
-			
-			
+			// 1- Creation d'un fichier et envoi
+			String objetEnvoi = "Hello world !!";
+			FileOutputStream fos = new FileOutputStream("fichier.temp");			
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			GZIPOutputStream gzout = new GZIPOutputStream(oos);			
+			oos.writeObject(objetEnvoi);
+			oos.close();
+			fos.close();
+
+			// 2- Reception (& //decompression) pour affichage
+			FileInputStream fis  = new FileInputStream("fichier.temp");
+			ObjectInputStream ois = new ObjectInputStream(fis);	
+			if (error) 
+			{
+				GZIPInputStream gzin = new GZIPInputStream(ois);
+			}
+			String objetRecu = (String) ois.readObject();
+			ois.close();
+			fis.close();
 			
 			// Si l'erreur apparait comme prévue, le message suivant n'apparaitra pas :
-			System.out.println("\nCa roule mec ?\n");
+			System.out.println(objetRecu);
 			
 		} catch (Exception e) {
 			if (viewBloc) 
@@ -124,19 +200,39 @@ public class Principal {
 			
 			shell.ErrorRedirect.Reflexivite();			
 			shell.ErrorRedirect.RedErreurs(e,ErrorRedirect.c,ErrorRedirect.m);
+			
+			System.out.println("Oups, reessayont autrement !");
 
-		} 
+		} finally {	//Traitements pour terminer proprement
+			if (viewBloc) 
+				System.out.println("FINALLY");
+			
+			if (!error) {
+				try {
+					test2(true);
+				} catch (Exception e) {
+					if (viewBloc) 
+						System.out.println("CATCH");
+					
+					shell.ErrorRedirect.Reflexivite();			
+					shell.ErrorRedirect.RedErreurs(e,ErrorRedirect.c,ErrorRedirect.m);
+				}
+			}	
+			
+		}
 	}
 	
 	// ERREUR CUSTOM PAR LE DEVELOPPEUR	
 	public static void test3(String chaine) throws NombreException {
-		System.out.println("\n--------------------------\nMETHODE 3");
+		System.out.println("\n--------------------------\nMETHODE 1/4");
 		try {
 			if (viewBloc) 
 				System.out.println("TRY");	
 			
 			if (chaine.equals("") == true)
 				throw new NombreException("Nombre erronée : chaine vide !");
+			else
+				System.out.println(chaine);
 			
 		} catch (Exception e) {
 			if (viewBloc) 
@@ -144,8 +240,12 @@ public class Principal {
 			
 			shell.ErrorRedirect.Reflexivite();			
 			shell.ErrorRedirect.RedErreurs(e,ErrorRedirect.c,ErrorRedirect.m);
-
-		}
+			
+			System.out.println("Oups, reessayont autrement !");
+			
+			// RESOLUTION ERREUR :
+			test3("Hello World !");
+		} 
 	}
 }
 
